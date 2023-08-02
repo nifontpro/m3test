90538aca0dab61fe3c82718c15e7a90aab58e6c9
Nifont <nifontbus@yandex.ru>
2023-08-02 19:31:43 +0300

    tb

--- app/src/main/java/ru/nb/m3test/MainActivity.kt
+++ app/src/main/java/ru/nb/m3test/MainActivity.kt
@@ -3,6 +3,10 @@ package ru.nb.m3test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import ru.nb.m3test.ui.theme.M3testTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +14,9 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			M3testTheme {
-				MainScreen()
+				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+					MainScreen()
+				}
 			}
 		}
 	}
--- app/src/main/java/ru/nb/m3test/MainScreen.kt
+++ app/src/main/java/ru/nb/m3test/MainScreen.kt
@@ -1,6 +1,7 @@
 package ru.nb.m3test
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,12 +13,13 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
@@ -35,7 +37,7 @@ fun MainScreen() {
 			.fillMaxSize()
 			.nestedScroll(scrollBehavior.nestedScrollConnection),
 		topBar = {
-			TopAppBar(
+			LargeTopAppBar(
 				scrollBehavior = scrollBehavior,
 				navigationIcon = {
 					IconButton(onClick = { /*TODO*/ }) {
@@ -45,7 +47,17 @@ fun MainScreen() {
 						)
 					}
 				},
-				title = { Text(text = "Main") },
+				title = {
+					Row(verticalAlignment = Alignment.CenterVertically) {
+						IconButton(onClick = { /*TODO*/ }) {
+							Icon(
+								imageVector = Icons.Default.FavoriteBorder,
+								contentDescription = "Mark as favorite"
+							)
+						}
+						Text(text = "Main")
+					}
+				},
 //				colors = TopAppBarDefaults.smallTopAppBarColors(
 //					containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp)
 //				)
